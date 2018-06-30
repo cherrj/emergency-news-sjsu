a4de1d7e55a8e93497cf7138e077897442da6991
Xi <xi@Xis-MacBook-Air.local>
2018-06-29 18:06:06 -0700

    Add all my files

--- app/src/main/java/com/example/xi/news_recommendation_feature/MainActivity.java
+++ app/src/main/java/com/example/xi/news_recommendation_feature/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.xi.news_recommendation_feature;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mCondtionTextView;
+    Button mButtonSunny;
+    Button mButtonFoggy;
+
+    DatabaseReference mRootRef= FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mconditionRef=mRootRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCondtionTextView=(TextView) findViewById(R.id.condition);
+        mButtonFoggy=(Button) findViewById(R.id.foggy);
+        mButtonSunny=(Button) findViewById(R.id.sunny);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mconditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mCondtionTextView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
